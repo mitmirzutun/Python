6cee943cf924233c89859bb728b5e710844f608a
mitmirzutun <milan1111@gmx.de>
2016-02-13 23:06:09 +0100

    Quiz a0.0.2.1
    Smaller Language-Update

--- Quiz a0.0.0.0.1.2.py
+++ Quiz a0.0.0.0.1.2.py
@@ -5,10 +5,10 @@ def punktzahl(pluspunkte):
 def ueberpruefen(antwort1,antwort2,benutzerantwort):
     if benutzerantwort != antwort1 and benutzerantwort!= antwort2:
         punktzahl(-1)
-        print("Das war die falsche Antwort. Die richtige Antwort war", antwort1)
+        print("That was the false answer. The correct answer was", antwort1)
     else:
         punktzahl(1)
-        print("Das war die richtige Antwort")
+        print("That was the right answer.")
 loesung1=input("""9/11 was a
 A = terrorist action
 B = fatal accident 
@@ -16,7 +16,7 @@ C = airline
 D = company
 E = island
 F = game
-Deine Antwort: """)
+Your answer: """)
 ueberpruefen("A", "a", loesung1)
 loesung2=input("""Who started World War II?
 A = The US
@@ -25,19 +25,19 @@ C = Russia
 D = Stalingrad
 E = Stalin
 F = Hitler/Germany
-Deine Antwort: """)
+Your answer: """)
 ueberpruefen("F", "f", loesung2)
 loesung3=input("""Who is Putin?
 A = President of the US
 B = President of Canada
 C = Dictator of Russia
-Deine Antwort: """)
+Your Answer: """)
 ueberpruefen("C", "c", loesung3)
 loesung4=input("""When did the US declare Independence
 A = 1990
 B = 16th century
 C = 1776
-Deine Antwort: """)
+Your answer: """)
 ueberpruefen("C", "c", loesung4)
 print("Du hast", punkte, "Punkte ereicht.")
 
